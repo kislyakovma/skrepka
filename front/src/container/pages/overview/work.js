536a16e202546c57b2aa4a9ca78d618f9174ad71
kislyakovm <kislyakovoffical@gmail.com>
2020-12-26 15:24:32 +0300

    user company modal

--- front/src/container/pages/overview/work.js
+++ front/src/container/pages/overview/work.js
@@ -1,35 +1,49 @@
 import React, { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom';
-import {BasicFormWrapper} from '../../styled';
-import {Button} from '../../../components/buttons/buttons';
 import { useDispatch, useSelector } from 'react-redux';
-import Heading from '../../../components/heading/heading';
-import { Col, DatePicker, Form, Input, Radio, Row, Select, Spin, Upload, notification, AutoComplete} from 'antd';
-
-const dateFormat = 'MM/DD/YYYY';
+import { Col, Modal, Form, Input, Table, Row, Select, Spin, Upload, notification, AutoComplete, Empty } from 'antd';
+import { BasicFormWrapper, Main, TableWrapper } from '../../styled';
+import { Button } from '../../../components/buttons/buttons';
+import { Cards } from '../../../components/cards/frame/cards-frame';
+import { companyPull } from '../../../redux/company/actionCreator';
 
 const Work = () => {
+  const dispatch = useDispatch();
   const [form] = Form.useForm();
 
-  const { isLoading} = useSelector((state) => {
-    return { 
+  const { isLoading, user, companies } = useSelector(state => {
+    return {
       isLoading: state.crud.loading,
+      user: state.auth.user,
+      companies: state.company.data,
     };
   });
 
   const [state, setState] = useState({
     values: '',
     inn: '',
-    address:'',
+    address: '',
     companies: '',
     kpp: '',
     bik: '',
     rs: '',
     bank: '',
+    visible: false,
   });
 
   const handleSubmit = values => {
     setState({ ...state, values });
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
   };
 
   useEffect(() => {
@@ -37,11 +51,16 @@ const Work = () => {
       address: state.address,
       companies: state.companies,
       kpp: state.kpp,
-      inn: state.inn
-    })
+      inn: state.inn,
+    });
   }, [state.address]);
-  
-  const getCompany = (query) => {
+
+  useEffect(() => {
+    // dispatch(companyPull());
+    console.log(companies);
+  }, []);
+
+  const getCompany = query => {
     const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party';
     const token = '6fcdc1c4c6caa6c32fb55b731b2bae0816c9f188';
     const options = {
@@ -50,107 +69,123 @@ const Work = () => {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        Authorization: 'Token ' + token,
+        Authorization: `Token ${token}`,
       },
-      body: JSON.stringify({ query: query }),
+      body: JSON.stringify({ query }),
     };
 
     fetch(url, options)
-      .then((response) => response.json())
-      .then((result) => setState({ ...state, searchSuggest: result.suggestions }))
+      .then(response => response.json())
+      .then(result => setState({ ...state, searchSuggest: result.suggestions }))
 
-      .catch((error) => console.log('error', error));
+      .catch(error => console.log('error', error));
   };
 
+  const columns = [
+    {
+      title: 'Компания',
+      dataIndex: 'company',
+      key: 'company',
+    },
+    {
+      title: 'ИНН',
+      dataIndex: 'inn',
+      key: 'inn',
+    },
 
+    {
+      title: 'Действия',
+      dataIndex: 'action',
+      key: 'action',
+      width: '90px',
+    },
+  ];
 
   return (
-    <Row justify="center">
-      <Col xl={10} md={16} xs={24}>
-        <div className="user-work-form">
-          <BasicFormWrapper>
-          <Form
-                            className="add-record-form"
-                            style={{ width: '100%' }}
-                            layout="vertical"
-                            form={form}
-                            name="edit"
-                            onFinish={handleSubmit}
-                            
-                          >
+    <Main>
+      <Row gutter={15}>
+        <Col className="w-100" md={24}>
+          <Cards headless>
+            <div>
+              <TableWrapper className="table-data-view table-responsive">
+                <Table
+                  dataSource={[]}
+                  columns={columns}
+                  pagination={false}
+                  locale={{ emptyText: <Empty description={false} /> }}
+                  loading={isLoading}
+                />
+              </TableWrapper>
+            </div>
+          </Cards>
+        </Col>
+      </Row>
+      <Modal visible={state.visible}>
+        <Row justify="center">
+          <Col xl={10} md={16} xs={24}>
+            <div className="user-work-form">
+              <BasicFormWrapper>
+                <Form
+                  className="add-record-form"
+                  style={{ width: '100%' }}
+                  layout="vertical"
+                  form={form}
+                  name="edit"
+                  onFinish={handleSubmit}
+                >
+                  <Form.Item name="inn" label="ИНН">
+                    <AutoComplete
+                      options={state.searchSuggest}
+                      allowClear
+                      filterOption={false}
+                      notFoundContent={<p>Введите ИНН</p>}
+                      placeholder="ИНН"
+                      onSearch={e => {
+                        getCompany(e);
+                      }}
+                      onSelect={(e, option) => {
+                        setState({
+                          ...state,
+                          address: option.data.address.value,
+                          companies: e,
+                          kpp: option.data.kpp,
+                          inn: option.data.inn,
+                        });
+                      }}
+                    />
+                  </Form.Item>
+                  <Form.Item name="address" label="Адрес компании">
+                    <Input placeholder="Адрес компании" />
+                  </Form.Item>
+                  <Form.Item name="companies" label="Название компании">
+                    <Input placeholder="Название компании" defaultValue={state.companies} />
+                  </Form.Item>
+                  <Form.Item name="kpp" label="КПП">
+                    <Input placeholder="КПП" defaultValue={state.kpp} />
+                  </Form.Item>
+                  <Form.Item name="bik" label="Бик банка">
+                    <Input placeholder="Бик банка" defaultValue={state.bik} />
+                  </Form.Item>
+                  <Form.Item name="bank" label="Название Банка">
+                    <Input placeholder="Название Банка" defaultValue={state.bank} />
+                  </Form.Item>
+                  <Form.Item name="rs" label="Расчетный счет">
+                    <Input placeholder="Расчетный счет" defaultValue={state.rs} />
+                  </Form.Item>
 
-                            <Form.Item name="inn" label="ИНН">
-                              <AutoComplete
-                              options={state.searchSuggest}
-                              allowClear={true}
-                              filterOption={false}
-                              notFoundContent={<p>Введите ИНН</p>}
-                              placeholder='ИНН'
-                              onSearch={(e) =>{
-                                getCompany(e);
-                              }}
-                              onSelect={(e, option) =>{
-                                setState({ ...state, 
-                                  address: option.data.address.value, 
-                                  companies: e,
-                                  kpp: option.data.kpp,
-                                  inn: option.data.inn
-                                })
-                              }}
-                              />
-                            </Form.Item>
-                            <Form.Item name="address" label="Адрес компании">
-                              <Input
-                              placeholder='Адрес компании'
-                              />
-                            </Form.Item>
-                            <Form.Item name="companies" label="Название компании">
-                            <Input
-                            placeholder='Название компании'
-                              defaultValue={state.companies}
-                              />
-                            </Form.Item>
-                            <Form.Item name="kpp" label="КПП">
-                            <Input
-                            placeholder='КПП'
-                              defaultValue={state.kpp}
-                              />
-                            </Form.Item>
-                            <Form.Item name="bik" label="Бик банка">
-                            <Input
-                            placeholder='Бик банка'
-                              defaultValue={state.bik}
-                              />
-                            </Form.Item>
-                            <Form.Item name="bank" label="Название Банка">
-                            <Input
-                            placeholder='Название Банка'
-                              defaultValue={state.bank}
-                              />
-                            </Form.Item>
-                            <Form.Item name="rs" label="Расчетный счет">
-                            <Input
-                            placeholder='Расчетный счет'
-                              defaultValue={state.rs}
-                              />
-                            </Form.Item>
-
-                            
-                           
-
-                            <div className="record-form-actions text-right">
-                              <Button htmlType="submit" type="primary">
-                                {isLoading ? 'Загрузка...' : 'Обновить'}
-                              </Button>
-                            </div>
-                          </Form>
-          </BasicFormWrapper>
-        </div>
-      </Col>
-    </Row>
+                  <div className="record-form-actions text-right">
+                    <Button htmlType="submit" type="primary">
+                      {isLoading ? 'Загрузка...' : 'Обновить'}
+                    </Button>
+                  </div>
+                </Form>
+              </BasicFormWrapper>
+            </div>
+          </Col>
+        </Row>
+      </Modal>
+    </Main>
   );
 };
-
-
 
 export default Work;
--- front/src/container/pages/style.js
+++ front/src/container/pages/style.js
@@ -1,5 +1,256 @@
 import Styled from 'styled-components';
 
+const TableWrapper = Styled.div`
+    .ant-pagination-prev, .ant-pagination-next {
+        line-height: 28px !important;
+        transform: rotateY(${({ theme }) => (theme.rtl ? '180deg' : '0deg')})
+    }
+    .ant-table table{
+        text-align: ${({ theme }) => (!theme.rtl ? 'left' : 'right')};
+    }
+    .ant-table-thead > tr > th{
+        text-align: ${({ theme }) => (!theme.rtl ? 'left' : 'right')};
+    }
+    span.anticon.anticon-right{
+        transform: rotateY(${({ theme }) => (theme.rtl ? '180deg' : '0deg')})
+    }
+    span.anticon.anticon-left{
+        transform: rotateY(${({ theme }) => (theme.rtl ? '180deg' : '0deg')})
+    }
+    &.table-order,
+    &.table-seller,
+    &.table-data-view{
+        .ant-table-selection{
+            .ant-checkbox-indeterminate{
+                .ant-checkbox-inner{
+                    background: ${({ theme }) => theme['primary-color']};
+                    border-color: ${({ theme }) => theme['primary-color']};
+                    &:after{
+                        height: 2px;
+                        background-color: #fff;
+                    }
+                }
+            }
+        }
+        .ant-table-container{
+            padding-bottom: 25px;
+            border-bottom: 1px solid ${({ theme }) => theme['border-color-light']};
+        }
+        tbody{
+            tr{
+                &:hover{
+                    td{
+                        background: ${({ theme }) => theme['bg-color-light']};
+                    }
+                }
+                td{
+                    .product-id{
+                        max-width: 60px;
+                        text-align: ${({ theme }) => (theme.rtl ? 'left' : 'right')};
+                    }
+                }
+            }
+        }
+        .ant-pagination{
+            margin-top: 25px !important;
+        }
+    }
+    &.table-data-view{
+        .ant-table-container{
+            padding-bottom: 0;
+        }
+        table{
+            thead{
+                th{
+                    padding: 15px 16px;
+                }
+            }
+            tbody{
+                td{
+                    padding: 11px 16px;
+                    .record-img{
+                        img{
+                            max-width: 38px;
+                            border-radius: 50%;
+                            ${({ theme }) => (theme.rtl ? 'margin-left' : 'margin-right')}: 12px;
+                        }
+                    }
+                    .record-location{
+                        display: block;
+                        font-size: 12px;
+                        font-weight: 400;
+                        color: ${({ theme }) => theme['light-color']};
+                    }
+                    .status{
+                        font-weight: 500;
+                        text-transform: capitalize;
+                        &.active{
+                            color: ${({ theme }) => theme['success-color']};
+                            background: ${({ theme }) => theme['success-color']}10;
+                        }
+                        &.deactivated{
+                            color: ${({ theme }) => theme['warning-color']};
+                            background: ${({ theme }) => theme['warning-color']}10;
+                        }
+                        &.blocked{
+                            color: ${({ theme }) => theme['danger-color']};
+                            background: ${({ theme }) => theme['danger-color']}10;
+                        }
+                    }
+                    .table-actions{
+                        a{
+                            svg, i{
+                                width: 16px;
+                                color: ${({ theme }) => theme['extra-light-color']};
+                            }
+                            &.edit{
+                                ${({ theme }) => (theme.rtl ? 'margin-left' : 'margin-right')}: 6px;
+                              &:hover{
+                                svg,
+                                i{
+                                    color: ${({ theme }) => theme['info-color']};
+                                }
+                              }  
+                            }
+                            &.delete{
+                              &:hover{
+                                svg,
+                                i{
+                                    color: ${({ theme }) => theme['danger-color']};
+                                }
+                              }  
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    table{
+        thead{
+            tr{
+                border-radius: 10px;
+                th{
+                    &:last-child{
+                        text-align: ${({ theme }) => (theme.rtl ? 'left' : 'right')};
+                    }
+                    color: ${({ theme }) => theme['gray-color']};
+                    background: ${({ theme }) => theme['bg-color-light']};
+                    border-top: 1px solid ${({ theme }) => theme['border-color-light']};
+                    border-bottom: 1px solid ${({ theme }) => theme['border-color-light']};
+                    &:first-child{
+                        ${({ theme }) => (!theme.rtl ? 'border-left' : 'border-right')}: 1px solid ${({ theme }) =>
+    theme['border-color-light']};
+                        border-radius: ${({ theme }) => (!theme.rtl ? '10px 0 0 10px' : '0 10px 10px 0')} !important;
+                    }
+                    &:last-child{
+                        ${({ theme }) => (theme.rtl ? 'border-left' : 'border-right')}: 1px solid ${({ theme }) =>
+    theme['border-color-light']};
+                        border-radius: ${({ theme }) => (!theme.rtl ? '0 10px 10px 0' : '10px 0 0 10px')} !important;
+                    }
+                }
+            }
+        }
+        tbody{
+            tr{
+                &:hover{
+                    td{
+                        background: ${({ theme }) => theme['bg-color-light']};
+                    }
+                }
+                &.ant-table-row-selected{
+                    &:hover{
+                        td{
+                            background: ${({ theme }) => theme['bg-color-light']};
+                        }
+                    }
+                    td{
+                        background: ${({ theme }) => theme['bg-color-light']};
+                    }
+                }
+                td{
+                    border: 0 none;
+                    font-weight: 500;
+                    color: ${({ theme }) => theme['dark-color']};
+                    &:first-child{
+                        border-radius: ${({ theme }) => (!theme.rtl ? '10px 0 0 10px' : '0 10px 10px 0')} !important;
+                    }
+                    &:last-child{
+                        border-radius: ${({ theme }) => (!theme.rtl ? '0 10px 10px 0' : '10px 0 0 10px')} !important;
+                    }
+                    span{
+                        display: block;
+                    }
+                    .order-id{
+                        min-width: 128px;
+                    }
+                    .customer-name{
+                        min-width: 174px;
+                    }
+                    .status{
+                        min-width: 175px;
+                    }
+                    .ordered-amount{
+                        min-width: 175px;
+                    }
+                    .ordered-date{
+                        min-width: 165px;
+                    }
+                    .table-actions{
+                        min-width: 60px;
+                    }
+                }
+            }
+        }
+        .table-actions{
+            text-align: ${({ theme }) => (theme.rtl ? 'left' : 'right')};
+            min-width: 150px !important;
+            button{
+                height: 40px;
+                padding: 0 11px;
+                background: transparent;
+                border: 0 none;
+                color: ${({ theme }) => theme['extra-light-color']};
+                &:hover{
+                    &.ant-btn-primary{
+                        color: ${({ theme }) => theme['primary-color']};
+                        background: ${({ theme }) => theme['primary-color']}10;
+                    }
+                    &.ant-btn-info{
+                        color: ${({ theme }) => theme['info-color']};
+                        background: ${({ theme }) => theme['info-color']}10;
+                    }
+                    &.ant-btn-danger{
+                        color: ${({ theme }) => theme['danger-color']};
+                        background: ${({ theme }) => theme['danger-color']}10;
+                    }
+                }
+            }
+        }
+        .seller-info{
+            img{
+                ${({ theme }) => (theme.rtl ? 'margin-left' : 'margin-right')}: 12px;
+            }
+        }
+        .user-info{
+            display: flex;
+            align-items: center;
+            figure{
+                margin: 0 8px 0;
+            }
+            .user-name{
+                margin-bottom: 4px;
+                font-weight: 500;
+            }
+            .user-designation{
+                font-size: 12px;
+                font-weight: 400;
+                color: ${({ theme }) => theme['light-color']};
+            }
+        }
+    }    
+`;
+
 const ComingsoonStyleWrapper = Styled.div`
   text-align: center;
   background: #fff;
@@ -1221,4 +1472,6 @@ export {
   ErrorWrapper,
   AddUser,
   ComingsoonStyleWrapper,
+    TableWrapper,
+
 };
